d32c3e66e68b0a9eb0144d813a9174ec2da3109b
Anupam Mondal <a09051985@gmail.com>
2020-04-19 03:32:37 +0530

    190420200332am

--- abcd.js
+++ abcd.js
@@ -67,6 +67,29 @@
             var str =String.fromCharCode(i);
             document.getElementById("capital-abcd").innerHTML = str;
             i = i + 1;
+    
+            var ascnum = document.getElementById("ascii-val").value;
+            $.getJSON("picurl.json", function (data) {
+                $.each(data, function (key, value) {
+    
+                    var ascval = value.asciinum;    
+                    if (ascval === ascnum) {
+                        $( "#piclink1" ).attr( "src", value.pic1 ).appendTo( "#piclink1" );
+                        $( "#piclink2" ).attr( "src", value.pic2 ).appendTo( "#piclink2" );
+                        $( "#piclink3" ).attr( "src", value.pic3 ).appendTo( "#piclink3" );
+                        $( "#piclink4" ).attr( "src", value.pic4 ).appendTo( "#piclink4" );
+                        $( "#piclink5" ).attr( "src", value.pic5 ).appendTo( "#piclink5" );
+                        $( "#piclink6" ).attr( "src", value.pic6 ).appendTo( "#piclink6" );
+                        $( "#alt-name1" ).text( value.txt1 ).appendTo( "#alt-name1" );
+                        $( "#alt-name2" ).text( value.txt2 ).appendTo( "#alt-name2" );
+                        $( "#alt-name3" ).text( value.txt3 ).appendTo( "#alt-name3" );
+                        $( "#alt-name4" ).text( value.txt4 ).appendTo( "#alt-name4" );
+                        $( "#alt-name5" ).text( value.txt5 ).appendTo( "#alt-name5" );
+                        $( "#alt-name6" ).text( value.txt6 ).appendTo( "#alt-name6" );
+                    }
+        
+                });
+            });
         }
         
     }
@@ -108,6 +131,29 @@
             var str =String.fromCharCode(i);
             document.getElementById("capital-abcd").innerHTML = str;
             i = i - 1;
+    
+            var ascnum = document.getElementById("ascii-val").value;
+            $.getJSON("picurl.json", function (data) {
+                $.each(data, function (key, value) {
+    
+                    var ascval = value.asciinum;    
+                    if (ascval === ascnum) {
+                        $( "#piclink1" ).attr( "src", value.pic1 ).appendTo( "#piclink1" );
+                        $( "#piclink2" ).attr( "src", value.pic2 ).appendTo( "#piclink2" );
+                        $( "#piclink3" ).attr( "src", value.pic3 ).appendTo( "#piclink3" );
+                        $( "#piclink4" ).attr( "src", value.pic4 ).appendTo( "#piclink4" );
+                        $( "#piclink5" ).attr( "src", value.pic5 ).appendTo( "#piclink5" );
+                        $( "#piclink6" ).attr( "src", value.pic6 ).appendTo( "#piclink6" );
+                        $( "#alt-name1" ).text( value.txt1 ).appendTo( "#alt-name1" );
+                        $( "#alt-name2" ).text( value.txt2 ).appendTo( "#alt-name2" );
+                        $( "#alt-name3" ).text( value.txt3 ).appendTo( "#alt-name3" );
+                        $( "#alt-name4" ).text( value.txt4 ).appendTo( "#alt-name4" );
+                        $( "#alt-name5" ).text( value.txt5 ).appendTo( "#alt-name5" );
+                        $( "#alt-name6" ).text( value.txt6 ).appendTo( "#alt-name6" );
+                    }
+        
+                });
+            });
         }
         
     }
